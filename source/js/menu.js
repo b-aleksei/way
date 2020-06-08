b45e792967b9b54c3314b48eac1dccd3716a3c12
Aleksei Bugaev <103301@mail.ru>
2020-06-08 09:25:21 +0300

    middle state

--- source/js/menu.js
+++ source/js/menu.js
@@ -34,3 +34,80 @@ places.addEventListener('click', function (e) {
 })
 
 })();
+
+//плавный скролл с главного экрана
+
+(function () {
+
+let anchor = document.querySelector(".main-bg__watch");
+
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    let getId = anchor.getAttribute("href");
+
+    if (getId) {
+      document.querySelector(getId).scrollIntoView({
+        behavior: "smooth",
+        block: "start"
+      })
+    }
+  })
+
+
+  let anchors = document.querySelectorAll(".scroll-to");
+  let previousTarget = document.createElement('div');
+
+  anchors.forEach(anchor => {
+
+    anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    let getId = anchor.getAttribute("href");
+
+    if (getId) {
+      previousTarget.style.display = 'none'
+      let target = document.querySelector(getId);
+      target.style.display = 'block';
+      target.scrollIntoView({
+        behavior: "smooth",
+        block: "start"
+      })
+      previousTarget = target;
+    }
+  })
+  })
+
+
+
+/*
+scroll-to - класс каждой ссылки анкора
+
+.to_top {
+  display: none;
+  position: fixed;
+  bottom: 30px;
+  right: 10px;
+  width: 40px;
+  height: 40px;
+  text-align: center;
+  line-height: 40px;
+  box-shadow: 0 0 5px 1px rgba(103, 58, 39, 0.8);
+  color: #ba0b11;
+  background-color: #ffecb8;
+  cursor: pointer;
+  border-radius: 5px;
+  z-index: 999;
+}
+
+.to_top:active {
+  color: #000000;
+}
+
+.to_top-show {
+  display: block;
+}
+*/
+
+})();
+
